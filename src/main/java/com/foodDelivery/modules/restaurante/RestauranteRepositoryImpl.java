7d54e77d72b3beab032dd46890d06b83bc7235ed
Felipe Lemos <felipewesley69@gmail.com>
2022-11-11 09:43:43 -0300

    Pesquisa criteria Restaurante

--- src/main/java/com/foodDelivery/modules/restaurante/RestauranteRepositoryImpl.java
+++ src/main/java/com/foodDelivery/modules/restaurante/RestauranteRepositoryImpl.java
@@ -1,10 +1,15 @@
 package com.foodDelivery.modules.restaurante;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
 
 import org.springframework.stereotype.Repository;
 
@@ -13,18 +18,32 @@ public class RestauranteRepositoryImpl implements RestauranteRepositoryCustom {
 
 	@PersistenceContext
 	private EntityManager maneger;
-	
+
 	@Override
 	public List<Restaurante> buscarRestaurantes(String nome, BigDecimal taxaFreteInicial, BigDecimal taxaFreteFinal) {
-		
-		var jpql = "from Restaurante where nome like :nome and taxaFrete between :taxaInicial and :taxaFinal";
-		
-		return maneger.createQuery(jpql, Restaurante.class)
-				.setParameter("nome", "%" + nome + "%")
-				.setParameter("taxaInicial", taxaFreteInicial)
-				.setParameter("taxaFinal", taxaFreteFinal)
-				.getResultList();
-		
+
+		CriteriaBuilder cb = maneger.getCriteriaBuilder();
+		CriteriaQuery<Restaurante> quey = cb.createQuery(Restaurante.class);
+		Root<Restaurante> restaurante = quey.from(Restaurante.class);
+
+		List<Predicate> predicates = new ArrayList<Predicate>();
+
+		if (nome != null) {
+			predicates.add(cb.like(cb.lower(restaurante.get("nome")), "%" + nome.toLowerCase() + "%"));
+		}
+
+		if (taxaFreteInicial != null) {
+			predicates.add(cb.greaterThanOrEqualTo(restaurante.get("taxaFrete"), taxaFreteInicial));
+		}
+
+		if (taxaFreteFinal != null) {
+			predicates.add(cb.lessThanOrEqualTo(restaurante.get("taxaFrete"), taxaFreteFinal));
+		}
+
+		quey.where(predicates.toArray(new Predicate[0]));
+
+		return maneger.createQuery(quey).getResultList();
+
 	}
-	
+
 }
